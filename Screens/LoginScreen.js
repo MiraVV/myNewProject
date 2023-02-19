0ce69ea751d10459643ca3569d882dc1a438f78a
Myroslava <v16m06a@gmail.com>
2023-02-19 17:28:33 +0100

    1-2 homeworks

---
+++ Screens/LoginScreen.js
@@ -0,0 +1,162 @@
+import React, { useState } from "react";
+import {
+  Dimensions,
+  Keyboard,
+  TouchableWithoutFeedback,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  Button,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Platform,
+} from "react-native";
+
+const initialState = {
+  email: "",
+  password: "",
+};
+
+export default function LoginScreen() {
+  const [keyboardShown, setKeyboardShown] = useState(false);
+  const [state, setState] = useState(initialState);
+
+  const hideKeyboard = () => {
+    setKeyboardShown(false);
+    Keyboard.dismiss();
+  };
+
+  const submitForm = () => {
+    Keyboard.dismiss();
+    console.log(state);
+    setState(initialState);
+  };
+
+  const handleEmailFocus = () => {
+    setKeyboardShown(true);
+  };
+  const handlePasswordFocus = () => {
+    setKeyboardShown(true);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={hideKeyboard}>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.image}
+          source={require("../assets/images/photoBG.png")}
+        >
+          <View
+            style={{
+              ...styles.formContainer,
+              marginBottom: keyboardShown ? -180 : 0,
+            }}
+          >
+            <KeyboardAvoidingView
+              behavior={Platform.OS === "ios" ? "padding" : "height"}
+            >
+              <View
+              // style={{
+              //   marginBottom: keyboardShown ? -50 : 0,
+              // }}
+              >
+                <Text style={styles.formTitle}>Войти</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder={"Адрес электронной почты"}
+                  onFocus={handleEmailFocus}
+                  value={state.email}
+                  onChangeText={(value) =>
+                    setState((prevState) => ({ ...prevState, email: value }))
+                  }
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder={"Пароль"}
+                  secureTextEntry={true}
+                  onFocus={handlePasswordFocus}
+                  onChangeText={(value) =>
+                    setState((prevState) => ({ ...prevState, password: value }))
+                  }
+                />
+              </View>
+            </KeyboardAvoidingView>
+            <TouchableOpacity activeOpacity={0.8} style={styles.btn}>
+              <Text style={styles.btnTitle}>Войти</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.login}>
+              <Text style={styles.loginText}>
+                Нет аккаунта? Зарегистрироваться
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    justifyContent: "flex-end",
+    resizeMode: "cover",
+  },
+  formContainer: {
+    backgroundColor: "#fff",
+    borderTopRightRadius: 25,
+    borderTopLeftRadius: 25,
+    alignItems: "center",
+    padding: 16,
+  },
+  formTitle: {
+    fontSize: 30,
+    marginTop: 32,
+    marginBottom: 33,
+    textAlign: "center",
+    fontWeight: "500",
+  },
+  input: {
+    borderWidth: 1,
+    width: 343,
+    backgroundColor: "#F6F6F6",
+    borderColor: "#E8E8E8",
+    marginBottom: 16,
+    borderRadius: 8,
+    height: 50,
+    color: "#212121",
+    padding: 16,
+    placeholderTextColor: "#BDBDBD",
+  },
+  btn: {
+    borderWidth: 1,
+    marginTop: 27,
+    borderRadius: 100,
+    backgroundColor: "#FF6C00",
+    borderColor: "transparent",
+    alignItems: "center",
+    padding: 16,
+    cursor: "pointer",
+    width: 343,
+  },
+  login: {
+    alignItems: "center",
+    marginTop: 16,
+    marginBottom: 66,
+    cursor: "pointer",
+  },
+  loginText: {
+    color: "#1B4371",
+    fontSize: 16,
+  },
+  btnTitle: {
+    color: "#ffffff",
+    fontSize: 16,
+  },
+});
---
+++ Screens/RegistrationScreen.js
@@ -0,0 +1,210 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const initialState = {
+  login: "",
+  email: "",
+  password: "",
+};
+export default function RegistrationScreen() {
+  const [value, setValue] = useState("");
+  const [state, setState] = useState(initialState);
+  const [active, setActive] = useState({
+    login: false,
+    email: false,
+    password: false,
+  });
+  const [keyboardShown, setKeyboardShown] = useState(false);
+  const hideKeyboard = () => {
+    setKeyboardShown(false);
+    Keyboard.dismiss();
+  };
+  const submitForm = () => {
+    Keyboard.dismiss();
+    console.log(state);
+    setState(initialState);
+  };
+
+  const handleLoginFocus = () => {
+    setActive(() => ({ login: true }));
+    setKeyboardShown(true);
+  };
+  const handleEmailFocus = () => {
+    setActive(() => ({ email: true }));
+    setKeyboardShown(true);
+  };
+  const handlePasswordFocus = () => {
+    setActive(() => ({ password: true }));
+    setKeyboardShown(true);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={hideKeyboard}>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.image}
+          source={require("../assets/images/photoBG.png")}
+        >
+          <KeyboardAvoidingView
+            behavior={Platform.OS == "ios" ? "padding" : "height"}
+          >
+            <View
+              style={{
+                ...styles.formContainer,
+                marginBottom: keyboardShown ? 50 : 0,
+              }}
+            >
+              <View>
+                <View style={styles.imageAdd}>
+                  <View style={styles.plusSign}>
+                    <Image source={require("../assets/images/add.png")} />
+                  </View>
+                </View>
+                <Text style={styles.formTitle}>Регистрация</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder={"Логин"}
+                  value={state.login}
+                  onChangeText={(value) =>
+                    setState((prevState) => ({ ...prevState, login: value }))
+                  }
+                  onFocus={handleLoginFocus}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder={"Адрес электронной почты"}
+                  value={state.email}
+                  onChangeText={(value) =>
+                    setState((prevState) => ({ ...prevState, email: value }))
+                  }
+                  onFocus={handleEmailFocus}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder={"Пароль"}
+                  value={state.password}
+                  onChangeText={(value) =>
+                    setState((prevState) => ({
+                      ...prevState,
+                      password: value,
+                    }))
+                  }
+                  onFocus={handlePasswordFocus}
+                />
+              </View>
+              <TouchableOpacity
+                activeOpacity={0.8}
+                style={styles.btn}
+                onPress={submitForm}
+              >
+                <Text style={styles.btnTitle}>Зарегистрироваться</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.login}>
+                <Text style={styles.loginText}>Уже есть аккаунт? Войти</Text>
+              </TouchableOpacity>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "flex-end",
+    resizeMode: "cover",
+  },
+  formContainer: {
+    backgroundColor: "#fff",
+    borderTopRightRadius: 25,
+    borderTopLeftRadius: 25,
+    alignItems: "center",
+  },
+  formContainerInput: {},
+  formTitle: {
+    fontSize: 30,
+    marginTop: 32,
+    marginBottom: 33,
+    textAlign: "center",
+    fontWeight: "500",
+  },
+  imageAdd: {
+    width: 120,
+    height: 120,
+    position: "relative",
+    backgroundColor: "#F6F6F6",
+    marginTop: -60,
+    marginLeft: "auto",
+    marginRight: "auto",
+    borderRadius: 16,
+  },
+  plusSign: {
+    position: "absolute",
+    right: -12,
+    bottom: 14,
+    width: 25,
+    height: 25,
+    borderColor: "#FF6C00",
+    backgroundColor: "#ffffff",
+    borderWidth: 1,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    borderWidth: 1,
+    backgroundColor: "#F6F6F6",
+    borderColor: "#E8E8E8",
+    marginBottom: 16,
+    borderRadius: 8,
+    height: 50,
+    color: "#212121",
+    padding: 16,
+    placeholderTextColor: "#BDBDBD",
+    width: 343,
+  },
+  btn: {
+    borderWidth: 1,
+    marginTop: 27,
+    borderRadius: 100,
+    backgroundColor: "#FF6C00",
+    borderColor: "transparent",
+    alignItems: "center",
+    padding: 16,
+    cursor: "pointer",
+    width: 343,
+  },
+  login: {
+    alignItems: "center",
+    marginTop: 16,
+    marginBottom: 66,
+    cursor: "pointer",
+  },
+  loginText: {
+    color: "#1B4371",
+    fontSize: 16,
+  },
+  btnTitle: {
+    color: "#ffffff",
+    fontSize: 16,
+  },
+});
